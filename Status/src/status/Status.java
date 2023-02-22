eae13fdbd37026d352c7271aff144b02bb86cf79
MadihaPervez <123577556+MadihaPervez@users.noreply.github.com>
2023-02-22 13:09:36 -0500

    Add midterm

---
+++ Status/src/status/Status.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+import java.util.Scanner;
+
+/** This class takes String input from user 
+ * and calls method statusDetail to print the 
+ * status of User-Rejected,Pending,Processing and Approved.
+ * if the input is not given in String, 
+ * program will accept and produce wrong results. 
+ * change the code to use enums which avoids String input 
+ * then print the status details. 
+ * @author srinivsi 
+ */
+public class Status {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter the user status code (zero,one,two,three) in string");
+        String code = in.next();
+        StausUser t= new StausUser();
+        switch (code) {
+            case "zero":
+                t.statusDetail(Level.ZERO);
+                break;
+            case "one":
+                t.statusDetail(Level.ONE); 
+                break;
+            case "two":
+                t.statusDetail(Level.TWO); 
+                break;
+            case "three":
+                t.statusDetail(Level.THREE);
+                break;
+            default:
+                System.out.println("NOT VALID CODE");
+        }
+                
+    }
+    
+}
